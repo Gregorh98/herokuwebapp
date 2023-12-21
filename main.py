ea5fa567b997d2e9b16f8a3cc81180f45568c948
Gregor <gregor.hastings@outlook.com>
2023-12-21 15:08:11 +0000

    Added port ref

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import waitress as waitress
 from flask import Flask
 app = Flask(__name__)
@@ -7,4 +9,5 @@ def hello_world():
    return 'Hello World'
 
 if __name__ == '__main__':
-   waitress.serve(app, host="0.0.0.0", port="8080")
+   port = int(os.environ.get("PORT", 8080))
+   waitress.serve(app, host="0.0.0.0", port=port)
