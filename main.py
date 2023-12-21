c60963b24667d3e96c60efabc592831da7cc8d93
Gregor <gregor.hastings@outlook.com>
2023-12-21 15:53:39 +0000

    Added links

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import os
 
 import waitress as waitress
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template("index.html")
+   links = [
+      {"name":"Index", "link":url_for("index")},
+      {"name": "Index", "link": url_for("index")},
+      {"name": "Index", "link": url_for("index")}
+   ]
+
+   return render_template("index.html", links=links)
+
 
 if __name__ == '__main__':
    port = int(os.environ.get("PORT", 8080))
